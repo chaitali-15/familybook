96d4d63366ab6ae65b43dd300695d9d3b2d530e5
chaitali-15 <79220278+chaitali-15@users.noreply.github.com>
2021-09-05 22:41:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 var images =
-["chaitali.jpg","girl.jpg", "me.jpg","profile.jpg"];
-var names=["Sourav Jain","Anshika Jain","Lakshya Jain","Akshita Jain"];
-var i=0; var numbers_of_family_member_in_array = 4;
+["girl.jpeg","chaitali.jpg", "me.jpg","profile.jpg"];
+var names=["Member1","Member2","Member3","Member4"];
+var i=1; var numbers_of_family_member_in_array = 4;
 function update()
 {
     
    
-    if (i == 3)
+    if (i == 4)
     {
         i=0;
     }
+   
     var updateimage= images[i];
     var updatename= names[i];
-    document.getElementById("family_member_image").scr=updateimage;
+    document.getElementById("family_member_image").src=updateimage;
     document.getElementById("family_member_name").innerHTML=updatename;i++;
 }
